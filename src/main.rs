a971acbaa58dc1b0007894babe15505dfafdc68b
DeerDean <db.li.apps@gmail.com>
2022-06-14 22:49:43 +0800

    p2p

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use once_cell::sync::Lazy;
+use libp2p::{
+    core::upgrade,
+    identity,
+    floodsub::{Floodsub, Topic},
+    noise::{Keypair, X25519Spec},
+    tcp::TokioTcpConfig,
+    noise::{NoiseConfig},
+    mdns::{Mdns},
+    mplex,
+    PeerId,
+    Transport,
+};
+use log::{info};
+use serde::{Deserialize, Serialize};
+use tokio::{sync::mpsc};
+
+const STORAGE_FILE_PATH: &str = "./recipes.json";
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("recipes"));
+
+type Recipes = Vec<Recipe>;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Recipe {
+    id: usize,
+    name: String,
+    ingredients: String,
+    instructions: String,
+    public: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ListMode {
+    ALL,
+    One(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListRequest {
+    mode: ListMode,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListResponse {
+    mode: ListMode,
+    data: Recipe,
+    receiver: String,
+}
+
+enum EventType {
+    Response(ListResponse),
+    Input(String),
+}
+
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    info!("Peer Id: {}", PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&KEYS)
+        .expect("can create auth keys");
+
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated()) // XX Handshake pattern, IX exists as well and IK - only XX currently provides interop with other libp2p impls
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    let mut behaviour = RecipeBehaviour {
+        floodsub: Floodsub::new(PEER_ID.clone()),
+        mdns: Mdns::new(Default::default())
+            .await
+            .expect("can create mdns"),
+        response_sender,
+    };
+
+    behaviour.floodsub.subscribe(TOPIC.clone());
+    
+}
